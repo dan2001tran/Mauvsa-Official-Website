edf6677f29d4f8215ba2900434c9480f1812034b
dan2001tran <dan2001tran@gmail.com>
2024-04-28 12:55:43 -0400

    - Fixed the Parralax effect

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import CultureScene from "../Scenes/CultureScene";
-import LeadershipScene from "../Scenes/Leadership";
-import LandingPage from "../Scenes/LandingPage";
+import React from 'react';
+import './Home.css';
+import CultureScene from '../Scenes/CultureScene';
+import LeadershipScene from '../Scenes/Leadership';
+import MacGroup from '../assets/macgroup.jpg';
 const Home = () => {
   return (
-    <div>
-      <LandingPage />
+    <div className="parralax-wrapper">
+      <header>
+        <img
+          src={MacGroup}
+          alt="An Image of all Attendees to MAC XII"
+          className="landing-image"
+        />
+        <a className="landing-image-title">
+          MID-ATLANTIC UNION OF VIETNAMESE STUDENT ASSOCIATION
+        </a>
+      </header>
       <CultureScene />
       <LeadershipScene />
     </div>
